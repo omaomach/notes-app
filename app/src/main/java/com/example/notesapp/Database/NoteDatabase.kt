683a9ea9e815b8d65163453288e6c1285eb9a16f
omaomach <aaronjoash44@gmail.com>
2023-04-22 05:30:45 +0300

    created abstract class NoteDatabase with singleton pattern

--- app/src/main/java/com/example/notesapp/Database/NoteDatabase.kt
+++ app/src/main/java/com/example/notesapp/Database/NoteDatabase.kt
@@ -1,4 +1,42 @@
 package com.example.notesapp.Database
 
-class NoteDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import com.example.notesapp.Models.Note
+
+// This class will be responsible for creating the database for the app. There should be one instance of the class
+// in the app
+@Database(entities = arrayOf(Note::class), version = 1, exportSchema = false)
+abstract class NoteDatabase {
+
+    abstract fun getNoteDao() : NoteDao
+
+    // this is the singleton pattern which implies that only one object of the class can be created
+    // if it is already created, it will return the reference to the same object
+    companion object {
+
+
+        @Volatile
+        private var INSTANCE : NoteDatabase? = null
+
+        fun getDatabase(context : Context) : NoteDatabase {
+
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    NoteDatabase::class.java,
+                    DATABASE_NAME
+                ).build()
+
+                INSTANCE = instance
+
+                instance
+
+            }
+
+        }
+
+    }
+
 }
